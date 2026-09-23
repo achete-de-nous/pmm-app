8a7102ff971b80352fd3612495a2bbe5c0eb38be
achete-de-nous <hello.achetedenous@gmail.com>
2026-09-23 17:48:57 +0700

    Delete pmm-app 2 directory

--- pmm-app 2/components/EmptyState.js
+++
@@ -1,8 +0,0 @@
-export default function EmptyState({ title, hint }) {
-  return (
-    <div className="border border-dashed border-gray-300 rounded-xl py-12 px-6 text-center text-gray-500">
-      <div className="font-medium text-ink mb-1">{title}</div>
-      {hint && <div className="text-sm">{hint}</div>}
-    </div>
-  );
-}
